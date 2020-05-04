f924ffa7209453dd22c9a37b92260266a8a5380e
miriam-sullivan <maljayd42@gmail.com>
2020-05-04 14:55:38 -0500

    i think im doing it right maybe?

--- index.js
+++ index.js
@@ -3,6 +3,38 @@ const express = require("express");
 const server = express();
 server.use(express.json());
 
+const shortid = require("shortid");
+
 server.listen(5000, () =>
   console.log("\n Api is running on localhost:5000 \n")
 );
+
+//user:
+
+let user = [
+  {
+    id: shortid.generate(),
+    name: "Bob Smith",
+    bio: "Just your average joe but more like your average Bob. Get it?",
+  },
+];
+
+//Ajax requests:
+
+server.post("/api/users", function (req, res) {
+  const newUser = req.body;
+  if (
+    user.name === null ||
+    user.bio === null ||
+    user.name === "" ||
+    user.bio === ""
+  ) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else if (!newUser) {
+    res
+      .status(500)
+      .json({ errorMessage: "The users information could not be retrieved." });
+  } else user.push(newUser) && res.status(201).json(newUser);
+});
